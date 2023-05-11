1b7dd3ecfbbd5b276d9f194ff321fdb0a6957d3f
ChungYuZ <mme99133@gmail.com>
2023-05-11 16:35:58 +0800

    week13

--- Final_Project/main.cpp
+++ Final_Project/main.cpp
@@ -25,41 +25,39 @@ void display()
         Rshoulder=glmReadOBJ("model/Rshoulder.obj");
         Rarm=glmReadOBJ("model/Rarm.obj");
     }
-    glPushMatrix();
-        glScalef(0.1,0.1,0.1);
-        if(show[0])glmDraw(head,GLM_MATERIAL);
+        glPushMatrix();
+            glTranslatef(teapotX,teapotY,0);
+            if(show[0])glmDraw(head,GLM_MATERIAL);
+        glPopMatrix();
         if(show[1])glmDraw(body,GLM_MATERIAL);
         if(show[2])glmDraw(Rshoulder,GLM_MATERIAL);
         if(show[3])glmDraw(Rarm,GLM_MATERIAL);
     glPopMatrix();
     glutSwapBuffers();
 }
+int oldX=0,oldY=0;
 void mouse(int button, int state, int x, int y)
 {
-    teapotX = (x-150)/150.0;
-    teapotY = (150-y)/150.0;
-    if(state==GLUT_DOWN){ ///如果mouse按下去
-        if(fout==NULL) fout = fopen("file4.txt", "w");
-
-        fprintf(fout, "%f %f\n", teapotX, teapotY);
+    if(state==GLUT_DOWN){
+        oldX=x;
+        oldY=y;
     }
-    display();
 }
-//void keyboard(unsigned char key, int x, int y)///step02-2
-//{
-//    if(fin==NULL){
-//       fclose(fout);///step02-2
-//        fin = fopen("file4.txt", "r");///step02-2
-//    }
-//    fscanf(fin, "%f%f", &teapotX, &teapotY);///step02-2
-//    display();///step02-2
-//}///step02-2
+void motion(int x,int y){
+    teapotX += (x-oldX)/150.0;
+    teapotY -= (y-oldY)/150.0;
+    oldX=x;
+    oldY=y;
+    glutPostRedisplay();
+}
+
 int main(int argc, char** argv)
 {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_DEPTH);
     glutCreateWindow("week13");
 
+    glutMotionFunc(motion);
     glutDisplayFunc(display);
     glutKeyboardFunc(keyboard); ///step02-2
     glutMouseFunc(mouse); ///step02-1
