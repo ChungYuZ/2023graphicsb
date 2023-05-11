f5c108c8dc53caaeafc1eff973a966703187fb76
ChungYuZ <mme99133@gmail.com>
2023-05-11 14:49:29 +0800

    week13

--- Final_Project/main.cpp
+++ Final_Project/main.cpp
@@ -1,44 +1,45 @@
-///week12-5_TRT_keyboard_mouse 要用 keyboard mouse 來操控
-#include <stdio.h>
+#include <stdio.h> ///要檔案的Input/Output
 #include <GL/glut.h>
-FILE * fout = NULL; ///step02-2 一開始,檔案沒有開, NULL
-FILE * fin = NULL; ///step02-2 要讀檔用的指標, 一開始也是 NULL
-float teapotX=0, teapotY=0; ///幫我們看移動值
-float angle=0, angle2=0, angle3=0;///step03-2 擺動作
+///step03-1
+///CodeBlocks的專案設定 Project-Properties裡第二個Build Target
+/// Executing working dir 工作執行目錄
+///原本是 C:\Users\Administrator\Desktop\freeglut\bin
+///改成 . (小數點) 再 File-Save Everything 便能將專案檔設好、存檔。
+///之後你的工作執行目錄, 就在你的程式專案的那一目錄裡
+///但執行時, 就會少了 freeglut.dll 檔, 你再手動copy到你的專案檔裡。
+///  註: 你可以看到 .cbp CodeBlocks Project 專案裡 working_dir 有變動哦
+float teapotX = 0, teapotY = 0;
+FILE * fout = NULL;///step02-1
+FILE * fin = NULL;///step02-2
 void display()
 {
     glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
     glPushMatrix();
-        ///glTranslatef(teapotX, teapotY, 0);
-        ///glTranslatef(????); ///step03-2 擺動作
-        glRotatef(angle, 0, 0, 1);///step03-2 擺動作
-        glTranslatef(0.400000, -0.060000, 0);///step03-2
-        ///glTranslatef(teapotX, teapotY, 0);///step03-2 擺動作
+        glTranslatef(teapotX, teapotY, 0);
         glutSolidTeapot( 0.3 );
     glPopMatrix();
     glutSwapBuffers();
 }
 void mouse(int button, int state, int x, int y)
 {
-    if(state==GLUT_DOWN){
-        teapotX = (x-150)/150.0;
-        teapotY = (150-y)/150.0;
-        angle = x;
-        printf("glTranslatef(%f, %f, 0);\n", teapotX, teapotY);
-        if(fout==NULL) fout = fopen("file4.txt", "w"); ///step02-2 沒開檔,就開
-        ///fprintf(fout, "%f %f\n", teapotX, teapotY); ///step02-2 要再存座標
+    teapotX = (x-150)/150.0;
+    teapotY = (150-y)/150.0;
+    if(state==GLUT_DOWN){ ///如果mouse按下去
+        if(fout==NULL) fout = fopen("file4.txt", "w");
+
+        fprintf(fout, "%f %f\n", teapotX, teapotY);
     }
     display();
 }
-void keyboard(unsigned char key, int x, int y) ///step02-2 keyboard函式
+void keyboard(unsigned char key, int x, int y)///step02-2
 {
-    if(fin==NULL){ ///step02-2 如果檔案還沒 fopen(), 就開它
-        fclose(fout); ///前面mouse會開fout指標, 所以要關掉
-        fin = fopen("file4.txt", "r"); ///step02-2 沒開檔,就開
+    if(fin==NULL){
+        fclose(fout);///step02-2
+        fin = fopen("file4.txt", "r");///step02-2
     }
-    ///fscanf(fin, "%f %f", &teapotX, &teapotY); ///step02-2 真的讀檔
-    display(); ///step02-2 重畫畫面
-}
+    fscanf(fin, "%f%f", &teapotX, &teapotY);///step02-2
+    display();///step02-2
+}///step02-2
 int main(int argc, char** argv)
 {
     glutInit(&argc, argv);
@@ -46,8 +47,8 @@ int main(int argc, char** argv)
     glutCreateWindow("week12");
 
     glutDisplayFunc(display);
-    glutMouseFunc(mouse);
-    glutKeyboardFunc(keyboard); ///step02-2 keyboard要做事囉(開檔、讀檔)
+    glutKeyboardFunc(keyboard); ///step02-2
+    glutMouseFunc(mouse); ///step02-1
 
     glutMainLoop();
 }
